d7972705ad4718c0f68074da84efe82e53f92816
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-09-20 14:13:00 -0500

    Gets Total Votes and runs error free

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,23 +4,23 @@ import csv
 csvpath = os.path.join( 'Resources','election_data.csv')
 
 # Create Function that passes election data
-def election_results(voter_data):
-    voter_ID = int(election_data[0])
-    county = str(election_data[1])
-    candidate = str(election_data[2])
+# def election_results(voter_data):
+#     voter_ID = int(election_data[0])
+#     county = str(election_data[1])
+#     candidate = str(election_data[2])
 
 # Create calculations that tally votes
-    total_votes=0
+total_votes=0
     
     # candidate_votes = 
 
-    print("Total Votes: "+str(total_votes))
+
     
 with open(csvpath,newline='') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     csv_header = next(csvreader)
 
     for row in csvreader:
-        
-
+        total_votes = total_votes+1
+print("Total Votes: "+str(total_votes))
 
