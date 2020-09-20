7bc7fabe7548934d0a1d19d786bfa95f4ed3a655
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-09-19 21:22:25 -0500

    Calculates Number of Months and Total Profit and Loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,9 +5,34 @@ import csv
 # Create Path to CSV file
 csvpath = os.path.join( 'Resources','budget_data.csv')
 
+# csv1 = pd.budget_data.csv("first_csv.csv")
 
-with open(csvpath) as csvfile:
+totalMonths=0
+totalProfLoss=0
+
+# grandTotal = 0
+
+# Skip first row when counting total Months
+
+with open(csvpath,newline='') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
+    csv_header = next(csvreader)
+# print(csv_header)
 
     for row in csvreader:
-        print(row[0]+","+row[1])
+# print(row[0]+","+row[1])
+# print(row)
+        totalMonths = totalMonths+1
+        totalProfLoss += int(row[1])
+print("Total Months: "+ str(totalMonths))
+print("Total: "+ str(totalProfLoss))
+        # if grandTotal.isdigit():
+        #     grandTotal = int(row[1])
+# grandTotal = [sum(row[1])for row in zip(csvreader)]
+# print(index)
+
+# grandTotal = grandTotal+int(row[1])
+
+# print("Total Months: "+ str(totalMonths))
+# print(grandTotal)
+# print(row[0])
